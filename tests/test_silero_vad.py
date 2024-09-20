c8bffd61f5adec45b7db5ab88a464a449d3142fe
daanzu <daanzu@gmail.com>
2024-09-20 05:30:16 -0400

    test: enhance process tests for SileroVAD to support various data types

--- tests/test_silero_vad.py
+++ tests/test_silero_vad.py
@@ -1,5 +1,6 @@
 import array
 import copy
+import ctypes
 import math
 
 import pytest
@@ -10,30 +11,41 @@ from silero_vad_lite import SileroVAD
 def silero_vad():
     return SileroVAD(16000)
 
-def test_silero_vad_process(silero_vad):
-    # Generate some dummy audio data
+def _generate_audio_data_array(silero_vad):
     num_samples = silero_vad.window_size_samples
-    sample_rate = 16000
+    sample_rate = silero_vad.sample_rate
     def audio_data_generator():
         for i in range(num_samples):
             t = i / sample_rate
             yield math.sin(2 * math.pi * 440 * t)
-    audio_data = array.array('f', audio_data_generator())
+    return array.array('f', audio_data_generator())
 
-    # Process the audio data
-    audio_data_orig = copy.deepcopy(audio_data)
+@pytest.mark.parametrize('sample_rate', [8000, 16000])
+@pytest.mark.parametrize('data_type', [array.array, bytes, bytearray, memoryview, ctypes.Array, list, tuple])
+def test_silero_vad_process(sample_rate, data_type):
+    silero_vad = SileroVAD(sample_rate)
+    audio_data = _generate_audio_data_array(silero_vad)
+    if data_type == array.array:
+        pass
+    elif data_type in [bytes, bytearray, memoryview, list, tuple]:
+        audio_data = data_type(audio_data)
+    elif data_type == ctypes.Array:
+        audio_data = (ctypes.c_float * len(audio_data))(*audio_data)
+    else:
+        raise ValueError(f"Invalid data type: {data_type}")
+    if data_type not in [memoryview, ctypes.Array]:
+        # test_silero_vad_process[memoryview-8000] - TypeError: cannot pickle memoryview objects
+        audio_data_orig = copy.deepcopy(audio_data)
     result = silero_vad.process(audio_data)
-
-    # Check if the result is a float between 0 and 1
     assert isinstance(result, float)
     assert 0 <= result <= 1
+    if data_type not in [memoryview, ctypes.Array]:
+        # test_silero_vad_process[Array-8000] - assert <silero_vad_l...x7fee623695b0> == <silero_vad_l...x7fee62369520>
+        assert audio_data == audio_data_orig
 
-    # Check that the data was not modified
-    assert audio_data == audio_data_orig
-
-def test_silero_vad_invalid_input(silero_vad):
+def test_silero_vad_process_invalid_input(silero_vad):
     with pytest.raises(TypeError):
-        silero_vad.process("invalid input")
+        silero_vad.process('invalid input')
     with pytest.raises(TypeError):
         silero_vad.process(1.0)
     with pytest.raises(ValueError):
